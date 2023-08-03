f61b2d792022a2b1323e8c8282e838425d42b768
Mohamed Bfs <lolhiamsimo@gmail.com>
2023-08-04 00:48:07 +0100

    15

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import Layout from '../components/Layout';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Login from '../components/auth/Login';
 // import Register from '../components/auth/Register';
+import Layout from '../components/Layout';
+import useAuth from '../hooks/useAuth';
 import classes from './Auth.module.scss';
 
 function Auth() {
+  const { auth } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth) {
+      navigate('/');
+    }
+  }, [auth, navigate]);
+
   return (
     <Layout>
       <div className={classes.form_container}>
         <Login />
-        { /* <Register /> */ }
+        {/* <Register /> */}
       </div>
     </Layout>
   );
